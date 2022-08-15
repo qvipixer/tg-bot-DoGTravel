ab698d722400e80d848b14354fed7229aa4e258a
AdminS <qvipixer@gmail.com>
2022-08-15 20:58:48 +0300

    BIG UP Travel

--- temp/main.py
+++
@@ -1,8 +0,0 @@
-from requests import get
-
-num = int(input())
-source = get(f"https://aws.random.cat/view/{num}").text
-if "id=\"cat" in source:
-    print(source.split("src=\"")[1].split("\"")[0])
-else:
-    print("Incorrect id")
